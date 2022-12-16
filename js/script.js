7c6ac7849c72c021cceee2d0e55590dd9a29da0b
unknown <pedroarcanjo113@gmail.com>
2022-12-16 14:49:14 -0300

    progeto em JavaScript

---
+++ js/script.js
@@ -0,0 +1,73 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigarório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (!checkEmail(emailValue)) {
+    setErrorFor(email, "Por favor, insira um email válido!.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória!");
+  } else if (passwordValue !== passwordConfirmationValue) {
+    setErrorFor(password, "As senhas não conferem!.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no mínimo sete caractéres!.");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigarória!.");
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas não conferem!.");
+  } else if (passwordConfirmationValue.length < 7) {
+    setErrorFor(
+      passwordConfirmation,
+      "A senha precisa ter no mínimo sete caractéres."
+    );
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Esse projeto foi feito para aprender a usar o javascript
